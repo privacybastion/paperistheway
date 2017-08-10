d85794f23181a6865d867cfa356af7c8737980b9
Ben Dechrai <ben@dechrai.com>
2017-08-11 07:08:03 +1000

    Added laravel app with new view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Paper Is The Way</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                margin: 0;
+            }
+
+            .content {
+                margin: 0 auto;
+                max-width: 800px;
+            }
+
+            h1 {
+                font-size: 3em;
+                color: #636b6f;
+            }
+
+            p {
+                font-size: 1.5em;
+            }
+
+            img {
+                width: 100%;
+            }
+
+        </style>
+    </head>
+    <body>
+            <div class="content">
+                <h1>Paper Is The Way</h1>
+
+                <p>Hello, Bastions of Privacy!</p>
+                <p><em>Paper Is The Way</em> was created to educate people about the privacy implications of the Australian 2016 Census. In order to avoid the invasive questions online, we advocated for completing the paper form, and only providing the information that you felt comfortabe sharing.</p>
+                <p><a href="https://www.facebook.com/privacybastion/">Our Facebook community</a> became a place where like-minded individuals also started to share their knowledge, thoughts and insights.</p>
+                <p>With the Census debacle over (for now?), we've rebranded to more accurately represent who we are.</p>
+                <p>Please join me in thanking <em>Paper Is The Way</em>, for bringing us this far, and welcoming <a href="https://privacybastion.com"><em>Privacy Bastion</em></a> as we continue the journey of defending our privacy, civil liberties, and data sovereignty.</p>
+                <p><a href="https://privacybastion.com"><img src="/images/rename.png"/></a></p>
+
+            </div>
+    </body>
+</html>
